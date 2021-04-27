9c4e5e7b5a139ebc181be671e0cfef8be9303f7f
wandersontx <wandersonh89@gmail.com>
2021-04-26 22:49:17 -0300

    integraçao com firebase

--- src/componentes/Historias/TextSelected.js
+++ src/componentes/Historias/TextSelected.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Text, Button } from 'react-native';
+import firebase from '../../firebaseConnection';
 
 export default function TextSelected({ route, navigation }) {
+
+  //implementação apenas para teste
+  const [nome, setNome] = useState('carregando...')
+  useEffect(() => {
+     async function getNome() {
+       await firebase.database().ref('nome').once('value', (snapshot) =>{
+         setNome(snapshot.val())
+       });
+     }
+
+     getNome();
+  },[]);
+
  return (
   <View>
     <Text style={{fontSize:18}} >Buscar historia de ID {route.params?.historia_id}</Text>
-
+    <Text> {nome} </Text>
     <Button 
     title="Ir para a tela de perguntas"
     onPress={() => navigation.navigate('Questions')}
---
+++ src/firebaseConnection.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDt3YVGUsRPCHadJuGb45O5ENmh8PFf--U",
+    authDomain: "appdeingles-26c62.firebaseapp.com",
+    projectId: "appdeingles-26c62",
+    storageBucket: "appdeingles-26c62.appspot.com",
+    messagingSenderId: "577347201242",
+    appId: "1:577347201242:web:662fc780fe007e72a6dde3",
+    measurementId: "G-GR1GR5GGH9"
+  };
+  
+  if(!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);  
+  }
+  
+  export default firebase;

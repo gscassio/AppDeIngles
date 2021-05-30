9b39daddda11042ff1a641b63c33eb819eb2227e
wandersontx <wandersonh89@gmail.com>
2021-05-30 15:11:57 -0300

    Logout

--- src/componentes/Perfil/index.js
+++ src/componentes/Perfil/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useContext} from 'react';
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import { AuthContext } from '../../contents/auth';
+import styles from './styles';
 
 export default function Perfil() {
+  const {user, signOut} = useContext(AuthContext);
+  
  return (
-   <View>
-       
+   <View style={styles.container}>
+      <TouchableOpacity style={styles.btnLogout} onPress={() => {signOut()}}>
+       <Image
+        style={styles.logout}
+        source={require('../../images/logout.png')}
+        />         
+       </TouchableOpacity>
+       <View style={styles.conteudo}>
+         <Text style={styles.txtNome}>{ user.nome }</Text>
+       </View>       
    </View>
   );
 }
---
+++ src/componentes/Perfil/styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container:{
+        flex: 1,
+         
+    },
+    txtNome:{
+        marginTop: '5%',
+        fontWeight: 'bold',
+        fontSize: 44,
+        color: '#1ABAA2',
+    },
+    logout:{
+        height: 40,
+        width: 40,        
+    },
+    btnLogout:{
+        marginTop: 8,
+        alignItems: 'flex-start',
+        marginLeft: 8,
+    },
+    conteudo:{
+        marginTop: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+
+});
